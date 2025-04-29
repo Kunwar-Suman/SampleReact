52b46aba6a4e388f3a57b88bfe52a2a151ad6c24
Suman Kunwar <sumankunwar8848@gmail.com>
2025-04-29 13:35:54 +0545

    added some basic css to Post Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import Post from "./components/Post";
 
 function App() {
-  return <Post />;
+  return <main>
+    <Post author="React" body="ReactJS is awesome!" />
+    <Post author="NextJS" body="Learn NextJS in Future!" />
+  </main>;
 }
 
 export default App;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,10 @@
-function Post() {
+import classes from'./Post.module.css';
+
+function Post(props) {
   return (
-    <div>
-      <p>Suman</p>
-      <p>React is awesome!</p>
+    <div className={classes.post}>
+      <p className={classes.author}>{props.author}</p>
+      <p className={classes.text}>{props.body}</p>
     </div>
   );
 }
