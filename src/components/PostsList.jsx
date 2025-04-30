5de80627db3fc5e17ca859644e852c0050d57d47
Suman Kunwar <sumankunwar8848@gmail.com>
2025-04-30 12:24:54 +0545

    added shared header with children prop

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,11 +1,12 @@
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
+import Modal from "./Modal";
 import { useState } from "react";
 
-function PostsList() {
-  const [enteredBody, setEnteredBody] = useState('');
-  const [enteredAuthor, setEnteredAuthor] = useState('');
+function PostsList({ isPosting, onStopPosting }) {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
 
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
@@ -15,11 +16,19 @@ function PostsList() {
   }
   return (
     <>
-    <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
-    <ul className={classes.posts}>
-      <Post author={enteredAuthor} body={enteredBody} />
-      <Post author="NextJS" body="Learn NextJS in Future!" />
-    </ul>
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
+
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="NextJS" body="Learn NextJS in Future!" />
+      </ul>
     </>
   );
 }
